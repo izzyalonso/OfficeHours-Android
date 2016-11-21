80fdc4b3fc934116605b98818f0f1f5764720ce6
Ismael Alonso <ismaha91@gmail.com>
2016-11-21 11:13:53 -0600

    Create add course layout

---
+++ app/src/main/java/org/tndata/officehours/activity/AddCourseActivity.java
@@ -0,0 +1,11 @@
+package org.tndata.officehours.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by ialonso on 11/21/16.
+ */
+public class AddCourseActivity extends AppCompatActivity{
+
+}
