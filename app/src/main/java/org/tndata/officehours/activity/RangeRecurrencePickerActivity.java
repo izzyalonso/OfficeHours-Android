1c3d58a2248bebf346f50365c95b8fbbbec60799
Ismael Alonso <ismaha91@gmail.com>
2016-11-29 11:50:39 -0600

    Add time picking capability

--- app/src/main/java/org/tndata/officehours/activity/RangeRecurrencePickerActivity.java
+++ app/src/main/java/org/tndata/officehours/activity/RangeRecurrencePickerActivity.java
@@ -1,6 +1,7 @@
 package org.tndata.officehours.activity;
 
 
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
@@ -9,9 +10,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import org.tndata.officehours.R;
 import org.tndata.officehours.databinding.ActivityRangeRecurrencePickerBinding;
+
+import java.util.Calendar;
 
 
 /**
@@ -19,8 +24,9 @@ import org.tndata.officehours.databinding.ActivityRangeRecurrencePickerBinding;
  *
  * @author Ismael Alonso
  */
-public class RangeRecurrencePickerActivity extends AppCompatActivity{
+public class RangeRecurrencePickerActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String MC_KEY = "org.tndata.officehours.RRP.MultipleChoice";
+
 
     public static Intent getIntent(@NonNull Context context, boolean multipleChoice){
         return new Intent(context, RangeRecurrencePickerActivity.class)
@@ -30,6 +36,13 @@ public class RangeRecurrencePickerActivity extends AppCompatActivity{
 
     private ActivityRangeRecurrencePickerBinding binding;
     private boolean multipleChoice;
+
+    private int fromHour;
+    private int fromMinute;
+
+    private int toHour;
+    private int toMinute;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
@@ -44,5 +57,53 @@ public class RangeRecurrencePickerActivity extends AppCompatActivity{
         else{
             binding.rrpCheck.setVisibility(View.GONE);
         }
+
+        binding.rrpFrom.setOnClickListener(this);
+        binding.rrpTo.setOnClickListener(this);
+
+        fromHour = -1;
+        fromMinute = -1;
+
+        toHour = -1;
+        toMinute = -1;
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.rrp_from:
+                selectTime(fromHour, fromMinute, binding.rrpFrom);
+                break;
+
+            case R.id.rrp_to:
+                selectTime(toHour, toMinute, binding.rrpTo);
+                break;
+        }
+    }
+
+    //TODO Haphazard!! I can and will do better than this when time allows
+    private void selectTime(int currentHour, int currentMinute, final EditText target){
+        Calendar calendar = Calendar.getInstance();
+        if (currentHour == -1){
+            currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+        }
+        if (currentMinute == -1){
+            currentMinute = calendar.get(Calendar.MINUTE);
+        }
+
+        new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute){
+                if (target == binding.rrpFrom){
+                    fromHour = hour;
+                    fromMinute = minute;
+                }
+                else if (target == binding.rrpTo){
+                    toHour = hour;
+                    toMinute = minute;
+                }
+                target.setText(hour + ":" + minute);
+            }
+        }, currentHour, currentMinute, true).show();
     }
 }
