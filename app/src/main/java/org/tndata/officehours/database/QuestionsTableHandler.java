fad85643bdb8c85d1d73b67669b65f945320d10a
Ismael Alonso <ismaha91@gmail.com>
2017-03-16 19:14:33 -0500

    Add getters to Question and database i/o methods

--- app/src/main/java/org/tndata/officehours/database/QuestionsTableHandler.java
+++ app/src/main/java/org/tndata/officehours/database/QuestionsTableHandler.java
@@ -1,8 +1,16 @@
 package org.tndata.officehours.database;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.support.annotation.NonNull;
 import org.tndata.officehours.database.DatabaseContract.QuestionEntry;
+import org.tndata.officehours.model.Question;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -19,10 +27,78 @@ public class QuestionsTableHandler extends TableHandler{
             + QuestionEntry.VOTES + " INTEGER, "
             + QuestionEntry.KEYWORDS + " TEXT)";
 
+    private static final String INSERT = "INSERT INTO " + QuestionEntry.TABLE + " ("
+            + QuestionEntry.CLOUD_ID + ", "
+            + QuestionEntry.TITLE + ", "
+            + QuestionEntry.CONTENT + ", "
+            + QuestionEntry.VOTES + ", "
+            + QuestionEntry.KEYWORDS + ") "
+            + "VALUES (?, ?, ?, ?, ?)";
+
+    private static final String SELECT = "SELECT * FROM " + QuestionEntry.TABLE;
+
+    private static final String TRUNCATE = "DELETE FROM " + QuestionEntry.TABLE;
+
 
     public QuestionsTableHandler(@NonNull Context context){
         init(context);
     }
 
+    public void saveQuestions(List<Question> questions){
+        SQLiteDatabase db = getDatabase();
+        db.beginTransaction();
+        SQLiteStatement stmt = db.compileStatement(INSERT);
 
+        for (Question question:questions){
+            //Previous bindings (if any) are emptied
+            stmt.clearBindings();
+
+            //Bindings
+            stmt.bindLong(1, question.getId());
+            stmt.bindString(2, question.getTitle());
+            stmt.bindString(3, question.getContent());
+            stmt.bindLong(4, question.getVotes());
+            stmt.bindString(5, question.getKeywordCsv());
+
+            //Execution
+            stmt.executeInsert();
+        }
+
+        //Close the transaction
+        db.setTransactionSuccessful();
+        db.endTransaction();
+
+        //Close the statement
+        stmt.close();
+    }
+
+    public List<Question> getQuestions(){
+        //Open a readable database and execute the query
+        SQLiteDatabase db = getDatabase();
+        Cursor cursor = db.rawQuery(SELECT, null);
+
+        ArrayList<Question> questions = new ArrayList<>();
+
+        //If there are rows in the cursor returned by the query
+        if (cursor.moveToFirst()){
+            //Populate the list of people
+            do{
+                List<String> keywords = Arrays.asList(getString(cursor, QuestionEntry.KEYWORDS).split(","));
+                questions.add(new Question(
+                        getLong(cursor, QuestionEntry.CLOUD_ID),
+                        getString(cursor, QuestionEntry.TITLE),
+                        getString(cursor, QuestionEntry.CONTENT),
+                        getInt(cursor, QuestionEntry.VOTES),
+                        keywords
+                ));
+            }
+            //Move on until the cursor is empty
+            while (cursor.moveToNext());
+        }
+
+        //Close the cursor
+        cursor.close();
+
+        return questions;
+    }
 }
--- model/src/main/java/org/tndata/officehours/model/Question.java
+++ model/src/main/java/org/tndata/officehours/model/Question.java
@@ -1,6 +1,7 @@
 package org.tndata.officehours.model;
 
 import android.os.Parcel;
+import android.support.annotation.NonNull;
 
 import com.google.gson.annotations.SerializedName;
 
@@ -24,8 +25,47 @@ public class Question extends Base{
     private List<String> keywords;
 
 
-    public Question(long id){
+    /**
+     * Constructor.
+     *
+     * @param id the id of the question.
+     * @param title the title of the question.
+     * @param content the content of the question.
+     * @param votes the number of positive votes of the question.
+     * @param keywords the keywords of the question.
+     */
+    public Question(long id, @NonNull String title, @NonNull String content, int votes,
+                    @NonNull List<String> keywords){
+
         super(id);
+        this.title = title;
+        this.content = content;
+        this.votes = votes;
+        this.keywords = keywords;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public int getVotes(){
+        return votes;
+    }
+
+    public List<String> getKeywords(){
+        return keywords;
+    }
+
+    public String getKeywordCsv(){
+        String result = "";
+        for (String keyword:keywords){
+            result += keyword + ",";
+        }
+        return result.substring(0, result.length()-1);
     }
 
     @Override
